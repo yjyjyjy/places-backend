a2e327d60ecf73e2b94e75772665b3749385959d
yjyjyjy <prowessyang@gmail.com>
2020-11-14 14:30:58 -0800

    add port env

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ mongoose
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@avalon.exhb8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((error) => console.log(error));
